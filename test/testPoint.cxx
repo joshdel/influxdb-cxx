acdf16c59ac910d05e5cacb2c9b802cea3389fe0
Joshua DelCastillo <joshua.delCastillo@borntec.com>
2020-05-12 20:48:32 -0400

    Fix missing iterator include

--- test/testPoint.cxx
+++ test/testPoint.cxx
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE Test InfluxDB Point
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
-
+#include <iterator>
 #include "../include/InfluxDBFactory.h"
 
 namespace influxdb {
